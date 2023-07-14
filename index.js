bdc7359c905df928e36bc44b35ec2c61d575d6ae
Monika <412.kri@gmail.com>
2023-07-14 12:18:36 +0300

    task 2

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ if (a >= b) {
 } else if (a <= b){
     console.log(b);
 }
+
+// 2. Naudojant for ciklą, išvesti į konsolę skaičius nuo 1 iki 10. (5 taškai)
+
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
