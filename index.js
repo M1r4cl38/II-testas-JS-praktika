1f1f5d83e9853a4ebd2e08b0d47773e2612ca97a
Monika <412.kri@gmail.com>
2023-07-14 12:09:14 +0300

    task 1

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Palyginti du skaičius a ir b. Išvesti į konsolę, kuris didesnis arba jie lygūs. (4 taškai)
+
+a = 10
+b = 77
+
+if (a >= b) {
+    console.log(a);
+} else if (a <= b){
+    console.log(b);
+}
